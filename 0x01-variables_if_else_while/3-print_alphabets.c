c131fc8821d1e6a6ae794822c97d4d0a2dc2bb9b
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-17 18:12:32 +0000

    Lower and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Success(0)
+ */
+int main(void)
+{
+	char x;
+	char X;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (X = 'A'; X <= 'Z'; X++)
+		putchar(X);
+	putchar('\n');
+	return (0);
+}

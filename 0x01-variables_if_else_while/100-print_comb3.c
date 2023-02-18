9ef86916152804d337efb41d21d13cb36caf26ba
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-18 00:13:54 +0000

    Puchar diffent combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Success(0)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar(48 + num1 % 10);
+			putchar(48 + num2 % 10);
+			if (num1 == 8 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

d2df106b24c7b20c86f68491988f4e888aff8249
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-18 12:35:02 +0000

    Putchar possible different possible combinations of three digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			putchar(48 + num1 % 10);
-			putchar(48 + num2 % 10);
+			putchar(48 + num1);
+			putchar(48 + num2);
 			if (num1 == 8 && num2 == 9)
 				continue;
 			putchar(',');

45ab6617a746caaf4c1b59948f5c5466b8f09c42
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-24 11:17:08 +0000

    Numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * more_numbers - prints numbers from 0 to 14 x10
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
+	int k;
+	int l;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			k = j / 10;
+			l = j % 10;
 			if (j >= 10)
-			{
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
-			}
-			else
-				_putchar(j + '0');
+				_putchar(k + '0');
+			if (j == 0)
+				l = 0;
+			_putchar(l + '0');
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }

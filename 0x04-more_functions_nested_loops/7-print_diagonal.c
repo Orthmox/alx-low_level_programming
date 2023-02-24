fe6b9cb710e9d157eaf4da8158699d946f1d6f49
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-24 21:49:21 +0000

    print 3 diagonally

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,15 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j <= i; j++)
+		if (n > 0)
 		{
-			if (n > 1)
+			for (j = 0; j <= i; j++)
 			{
 				_putchar(32);
 			}
+			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar(92);
-		_putchar('\n');
 	}
 	if (n <= 0)
 	{

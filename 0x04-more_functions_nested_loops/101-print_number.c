347ed18b1e93499b5439593e47cdb4fbbaed62e9
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-25 22:32:53 +0000

    Fixed type conversion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,16 +5,16 @@
  */
 void print_number(int n)
 {
-	unsigned int = n;
+	unsigned int num = n;
 
-	if (n < 0)
+	if (num < 0)
 	{
-		n = -(n);
+		num = -(num);
 		_putchar('-');
 	}
 	if (n / 10 > 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(num % 10 + '0');
 }

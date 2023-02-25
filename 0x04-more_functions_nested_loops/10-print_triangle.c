2adae08533c6be3a51b5f414b5adc323bc83b0f3
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-25 10:37:22 +0000

    Mirror me triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - print a right angle triangle
+ * @size: is the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	if (size <= 0)
+		_putchar('\n');
+}

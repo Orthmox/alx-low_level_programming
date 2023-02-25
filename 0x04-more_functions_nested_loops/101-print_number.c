3c3d4bf3e9066226592355e0d5492e9d96fc31cf
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-25 22:08:10 +0000

    Print an integer regardless with _putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: is the integer
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+		_putchar('-');
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n > 0 && n <= 9)
+		_putchar(n + '0');
+	if (n >= 10)
+	{
+		print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+}

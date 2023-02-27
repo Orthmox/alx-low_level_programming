2418a6fc253f1e4906b85616c6cd22eec807a820
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-27 06:32:45 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,33 +7,40 @@
 
 void print_times_table(int n)
 {
-	int i, k, prod;
+	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	if (n > 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-
-			for (k = 1; k <= n; k++)
+			for (j = 1; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = i * k;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
+				k = i * j;
+				if (k < 10)
 				{
-					_putchar((prod / 100) + '0'
-				else
-					_putchar((prod / 10) + '0');
-
-				_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				if (k >= 10 && k <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				if (k > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) / 10 + '0');
+					_putchar((k / 10) % 10 + '0');
+					_putchar((k % 10) % 10 + '0');
+				}
 			}
 			_putchar('\n');
 		}

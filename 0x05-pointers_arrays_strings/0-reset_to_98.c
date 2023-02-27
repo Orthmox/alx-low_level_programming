ed992d346dd9943ddcb118da9a4a5093eadd02d7
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-27 20:48:22 +0000

    Reset pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the stored value of a pointer to 98
+ * @n: is the pointer
+ */
+void reset_to_98(int *n)
+{
+	/* sets the stored value of the pointer to 98 */
+	*n = 98;
+}

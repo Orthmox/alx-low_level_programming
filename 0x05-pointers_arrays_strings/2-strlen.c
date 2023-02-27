3cd21018c35fe903576d7fed76c99f2870fbb4fe
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-27 23:11:57 +0000

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: is the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i + 1;
+	}
+	return (len);
+}

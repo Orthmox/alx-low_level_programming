58aaddf385a56a34329a24475f7760d2da524d1c
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-28 06:49:46 +0000

    Reverse that string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: is the string
+ */
+void print_rev(char *s)
+{
+	int i;
+	int len = 0;
+
+	while ( *s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (i = len + 1; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}

b4f0fa22900ad197a4e84cf770b4cd23aa34efe0
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-28 22:24:38 +0000

    Making integers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: is the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: is the the string
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	int i, len, sign;
+	unsigned int num;
+
+	len = _strlen(s);
+	sign = 1;
+	num = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (num * 10) + (s[i] - '0');
+		}
+	}
+	return (num * sign);
+}

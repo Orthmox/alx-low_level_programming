8ee30aa34a3caa26631adbe623d85b642f031276
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-28 15:24:48 +0000

    Print array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints the elements of an array
+ * @a: is the array
+ * @n: is the number of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf("%d", *a);
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", *(a + i));
+	}
+	printf("\n");
+}
+

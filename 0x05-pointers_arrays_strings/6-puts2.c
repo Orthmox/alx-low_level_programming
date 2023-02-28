557a73178bd0ca1423e460882f52b8731a28e9c2
Noble Adagbe <nobleadagbe@gmail.com>
2023-02-28 13:34:36 +0000

    First measure length

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,37 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string starting from the first
+ * _strlen - prints the length of a string
+ * @s: is the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+#include "main.h"
+/**
+ * puts2 - print every other character of a string starting from first
  * @str: is the string
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i, j, len;
+
+	len = _strlen(str);
+	j = len -1;
+
+	for (i = 0; *str != '\0' && i <= j; i = i + 2)
 	{
-		_putchar(*str);
-		str = str + 2;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }

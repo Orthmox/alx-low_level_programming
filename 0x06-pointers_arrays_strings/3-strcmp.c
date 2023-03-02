b304a8c039c111a2086478d74630e04b371e2108
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-02 17:22:20 +0000

    Change is constant

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,6 +8,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, change;
+
 	i = 0;
 
 	while (s1[i] != '\0' || s2[i] != '\0')
@@ -15,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 		change = s1[i] - s2[i];
 		if (s1[i] != s2[i])
 		{
-			return(change);
+			return (change);
 		}
 		i++;
 	}

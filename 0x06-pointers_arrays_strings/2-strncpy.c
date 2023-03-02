c7b8c76ae439b11a028af38709922c4ea8056603
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-02 13:16:47 +0000

    Copy this first

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while ((dest[i] != '\0' &&src[j] != '\0') && (j < n && i < n))
+	while (n--)
 	{
 		dest[i] = src[j];
 		j++;
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);

5fadbb8dba80d93f3a660b628464bf8aaf4cdbb7
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-06 20:48:58 +0000

    Spring in print

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -11,11 +11,11 @@ int main(void)
     _putchar('\n');
     print_number(402);
     _putchar('\n');
-    print_number(1024);
+    print_number(102466632);
     _putchar('\n');
     print_number(0);
     _putchar('\n');
-    print_number(-98);
+    print_number(-7778798);
     _putchar('\n');
     return (0);
 }
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,24 +5,26 @@
  */
 void print_number(int n)
 {
+	 unsigned int num = n;
+
 	if (n < 0)
 	{
-		n = -(n);
+		num = -(num);
 		_putchar('-');
 	}
-	if (n == 0)
+	if (num == 0)
 	{
 		_putchar('0');
 	}
-	if (n > 0 && n < 10)
+	if (num > 0 && num < 10)
 	{
-		_putchar(n + '0');
+		_putchar(num + '0');
 	}
-	if (n / 10)
+	if (num / 10 > 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 
-		_putchar(n % 10 + '0');
+		_putchar(num % 10 + '0');
 	}
 }
 

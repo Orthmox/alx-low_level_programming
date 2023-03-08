2afc15d28acb337200a6c50580127e357e211168
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-08 06:56:38 +0000

    Free my code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (*s != '\0')
-	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
+	while (*s != '\0' && s[i] != c)
+	{ 
 		i++;
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
 	}
 	return ('\0');
 }

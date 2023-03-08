538252f9dc38349c9bbc8f381c0fc83a3469df92
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-08 22:08:32 +0000

    Base to exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - calculates the value of a number raised to another number
+ * @x: is the base
+ * @u: is the exponent
+ * Return: result of computation
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}

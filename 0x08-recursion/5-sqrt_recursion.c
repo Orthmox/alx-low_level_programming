5a7092874905c38378443d7bb71e1f152c39e306
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-10 11:07:04 +0000

    Root all squares

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,28 @@
 #include "main.h"
 /**
+ * sqroot - finds the square root of a number
+ * @n: is the number
+ * @g: is the guess answer started from 1
+ * Return: g if square found, -1 if no square root found
+ */
+int sqroot(int n, int g)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+	if (n / g == g && n % g == 0)
+		return (g);
+	else if (g < n)
+		return (sqroot(n, g + 1));
+	return (-1);
+}
+/**
  * _sqrt_recursion - finds the square root of a number
  * @n: is the number
  * Return: the square root of n
  */
 int _sqrt_recursion(int n)
 {
-	int ans;
-	ans = (ans + (n / ans)) / 2;
-
-	if (ans * ans <= n && (ans + 1) * (ans + 1))
-		return (ans);
-	else
-		return (_sqrt_recursion((ans + n / ans) /2));
-
+	return (sqroot(n, 1));
 }

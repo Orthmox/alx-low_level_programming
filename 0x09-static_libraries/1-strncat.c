8890dcefa41c51de92e6abef7450d82632435c8d
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-11 17:02:37 +0000

    Add more files

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,20 @@
+/**
+ * _strncat - appends one string to another
+ * @dest: is the destination string
+ * @src: is the string to be copied
+ * @n: is the number of characters
+ * Return: a string this time
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[j++] = src[i];
+	}
+	return (dest);
+}

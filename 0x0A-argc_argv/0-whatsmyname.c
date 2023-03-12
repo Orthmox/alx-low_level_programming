d9722354c58ef340cb18020190d3bb84d1d16cca
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-12 07:29:22 +0000

    Print main argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is the entry point
+ * @argc: is the argument counter
+ * @argv: is the argument vector
+ * Return: Success (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

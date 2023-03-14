2451b264de2fae18cce0075ff3797ed94141536b
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-14 07:32:08 +0000

    Character initialized memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of characters
+ * @size: is the size of the array
+ * @c: is the character array is initialized to
+ * Return: pointer to array, NULL if fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	str = malloc(sizeof(char) * (size + 1));
+
+
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+
+	str[i] = '\0';
+	return (str);
+}
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#0x0B. C - malloc, free
+Contains tasks on of dynamic memory allocation using malloc in alx

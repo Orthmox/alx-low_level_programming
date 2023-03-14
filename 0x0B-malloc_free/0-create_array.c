4072232143e8f0d8dba95c4a6340ffddaf3eb4a0
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-14 07:44:09 +0000

    Correct malloc size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	str = malloc(sizeof(char) * (size + 1));
+	str = malloc(sizeof(char) * size);
 
 
 	while (i < size)

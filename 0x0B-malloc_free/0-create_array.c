841f237b10badc8419634aaf734047ffe6a6e68f
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-14 07:48:14 +0000

    Correct malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,9 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i = 0;
 
-	if (size == 0)
-		return (NULL);
-
 	str = malloc(sizeof(char) * size);
-
+	if (str == NULL)
+		return (NULL);
 
 	while (i < size)
 	{

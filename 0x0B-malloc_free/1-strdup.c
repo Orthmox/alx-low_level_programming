b12ff2e5a8cdca779c2e2c78fc91af1004943e1e
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-14 11:38:51 +0000

    String memory duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - computes the length of a string
+ * @s: is the string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
+ * _strup - stores a copy of a string in memory
+ * @str: is the string
+ * Return: a string else NULL if empty
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int len = _strlen(str);
+	char *new;
+
+	if (str == 0)
+		return (NULL);
+	new = malloc(sizeof(char) * len);
+	i = 0;
+	while (i < len)
+	{
+		new[i] = str[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}

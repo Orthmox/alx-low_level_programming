49f4ea01c965bdbf61ebd4a795247fe3ee118fd6
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-16 17:04:00 +0000

    Allocate size_t size

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,10 +3,14 @@
 /**
  * malloc_checked - allocates memory
  * @b: is the size of memory required
+ * Return: a pointer to allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	void *t = malloc(sizeof(unsigned int) * b);
+	size_t size = b;
+
+	void *t = malloc(size);
+
 	if (t == NULL)
 		exit(98);
 	return (t);

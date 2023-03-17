8f3e20e195bfbbf2049615ddc66914e04c26ec09
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-17 08:40:41 +0000

    Malloc size int

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,9 +15,9 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	size = (max - min) + 1;
+	size = max - min + 1;
 
-	ras = malloc(sizeof(ras) * size);
+	ras = malloc(sizeof(int) * size);
 	if (ras == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)

5d6f57c36684c36d1a33a0576f69f9923c00430f
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-17 07:21:02 +0000

    Memory int range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,7 +8,8 @@
  */
 int *array_range(int min, int max)
 {
-	int i size;
+	int i, size;
+	int j = 0;
 	int *ras;
 
 	if (min > max)
@@ -20,6 +21,6 @@ int *array_range(int min, int max)
 	if (ras == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
-		ras[i] = i;
+		ras[j++] = i;
 	return (ras);
 }

c28f362129684c22d02dacad9d077b8d1db334ac
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-25 21:11:31 +0000

    Handle null 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	struct dog *var;
 
 	if (d)
+	{
 		var = d;
-	var->name = name;
-	var->age = age;
-	var->owner = owner;
+		var->name = name;
+		var->age = age;
+		var->owner = owner;
+	}
 }

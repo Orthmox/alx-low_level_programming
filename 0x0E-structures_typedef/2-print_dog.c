6b446a4090837f3533db78229b3acfab0de965f3
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-25 22:27:59 +0000

    Null print nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,17 +9,16 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if ((*d).name)
+		if ((*d).name != NULL)
 		{
 			printf("Name: %s\n", (*d).name);
 		}
 		else if ((*d).name == NULL)
 			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-		if (d->owner)
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else if (d->owner == NULL)
 			printf("Owner: (nil)\n");

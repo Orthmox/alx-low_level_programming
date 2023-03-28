be958a5f217661015d70c08d3a56366b0ae5e223
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-28 20:30:29 +0000

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: is the name to be printed
+ * @f: is a pointer to a function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*gnap)(char *);
+	gnap = f;
+	gnap(name);
+}

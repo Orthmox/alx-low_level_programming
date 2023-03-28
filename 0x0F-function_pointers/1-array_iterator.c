10e8367e8397206db75e8fc74223d3c49e7ad5a7
Noble Adagbe <nobleadagbe@gmail.com>
2023-03-28 21:04:59 +0000

    Print array elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - prints elements of an array
+ * @array: is a pointer to the array
+ * @size: is the size of the array
+ * @action: is a pointer to the function to execute
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	void (*eap)(int) = action;
+
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			eap(array[i]);
+		}
+	}
+}

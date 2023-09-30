5f0a11554cb1e92612d8c99c1bdaee539964974a
Orthmox <nobleadagbe@gmail.com>
2023-09-30 16:36:13 +0000

    Arg Counter

---
+++ codetr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) 
+{
+	int x = 5;
+
+	if (argc > 3);
+		for (x > argc && x < 15);
+		{
+			printf("%d", argc[x]);
+		}
+	return (0);
+}
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int **p;
+
+	printf("Hello world\n");
+	printf("%ld\n", sizeof(**p));
+
+	return (0);
+}
